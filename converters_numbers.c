3b0e349e824c036136f1aa30814f121935fd58cf
JuanDavidPena <j_david0915@hotmail.com>
2021-07-28 18:12:27 +0000

    [MODIFY] edge case INT_MIN in converters_numbers.c

--- converters_numbers.c
+++ converters_numbers.c
@@ -14,26 +14,27 @@ unsigned int convert_di(va_list arg_list, buffer_t *output)
 {
 	int tens = 1, num = va_arg(arg_list, int), tensit = num;
 	unsigned int ret = 0;
-	char neg = '-', temp;
-	char *temp2;
+	char neg = '-', zero = '0', temp;
+	char *max = "2147483647", *min = "2147483648";
 
 	if (num < 0)
 	{
-		num *= -1;
 		_copy(output, &neg, 1);
+		if (num == INT_MIN)
+		{
+			_copy(output, min, 10);
+			return (11);
+		}
+		num *= -1;
 		tensit *= -1;
-		ret++;
 	}
 	if (num == 0)
 	{
-		temp = '0';
-		_copy(output, &temp, 1);
-		ret++;
+		_copy(output, &zero, 1);
 	}
 	if (num == INT_MAX)
 	{
-		temp2 = "2147483647";
-		_copy(output, temp2, 10);
+		_copy(output, max, 10);
 		return (10);
 	}
 	while (num != 0)
